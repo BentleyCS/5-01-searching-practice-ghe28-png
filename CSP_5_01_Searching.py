8e03b2a93781e5a53555c8fb4196ded057d928f2
ghe28-png <ghe28@bentleyschool.org>
2026-01-18 00:50:20 -0800

    Add files via upload

--- CSP_5_01_Searching.py
+++ CSP_5_01_Searching.py
@@ -1,19 +1,53 @@
 
+import random
 def randomSearch(items:list, target) -> int:
     #Modify the below function such that it takes in a list of items and a target value.
     #Randomly choose an item from the list and if it isn't the target value try again.
     #print out the amount of tries it took and return the index of the target value
+    j=0
+    while True:
+        i=random.randint(0,len(items)-1)
+        j+=1
+        if(items[i]==target):
+            print(j)
+            return i
     pass
 
 def linearSearch(items:list, target) ->tuple[int,int]:
     #Modify the below function such that it implements linear search.
     #Return the index of the target value and the amount of checks it took
     #if the value is not within the list return -1 as the index.
-    pass
+  check=0
+  i=0
+  while i<=len(items)-1:
+    i+=1
+    check+=1
+    if(items[i]==target):
+        return i,check
+  return -1
+pass
 
 
 def binarySearch(items:list, target) -> tuple[int,int]:
-    # Modify the below function such that it implements linear search.
+    # Modify the below function such that it binary search.
     # Return the index of the target value and the amount of checks it took
     # if the value is not within the list return -1 as the index.
+    low=0
+    check=0
+    high=len(items)-1
+    while low<=high:
+        mid=((items[low]+items[high])/2)
+        check += 1
+        if (items[mid]==target):
+            return mid, check
+        elif(items[mid]<target):
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+
+
+
+
+
     pass
